4996dbd56ec3c42ed945e77f36a9ba7bbfe00174
ryandeist <ryandeist@gmail.com>
2024-11-12 20:33:33 -0500

    Added middleware for authentication

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+// require our new middleware!
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -19,7 +23,7 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -28,21 +32,17 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
+app.use(passUserToView); // use new passUserToView middleware here
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
+//public routes
+app.get('/', (req, res) => {
+  res.render('index.ejs')
 });
 
 app.use('/auth', authController);
+
+//protected routes
+app.use(isSignedIn); // use new isSignedIn middleware here
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
